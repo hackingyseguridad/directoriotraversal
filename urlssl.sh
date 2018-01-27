6cb1c5e2951220bcd3690ac647b570448f0a92c5
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-01-27 22:34:55 +0100

    Update urlssl.sh

--- urlssl.sh
+++ urlssl.sh
@@ -6,6 +6,8 @@
 # -s --silent muestra en pantalla los datos solicitados
 # -vvv Mostrar detalle en el resultado
 #
+# la url entre comillas ""
+#
 #!/bin/bash
 echo "Uso.: ./urlssl.sh https://url"
 curl -k -s -vvv $1
