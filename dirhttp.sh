a300762a1ff67ee1e967b576fb5dc55ca4f356dd
hackingyseguridad <antonio_taboada@hotmail.es>
2019-01-11 08:50:39 +0100

    Update dirhttp.sh

--- dirhttp.sh
+++ dirhttp.sh
@@ -9,7 +9,7 @@
 #
 #!/bin/bash
 echo "Uso.: ./dirhttp.sh http://url"
-curl --insecure --silent -vvv --compressed \
+curl -0 --insecure --silent -vvv --compressed \
 -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
 -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
 -H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
