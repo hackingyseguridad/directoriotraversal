5698d8f59b194dcb800ac7609318ad3c0c0448e4
hackingyseguridad <antonio_taboada@hotmail.es>
2019-01-11 09:22:14 +0100

    Update urlssl.sh

--- urlssl.sh
+++ urlssl.sh
@@ -1,4 +1,4 @@
-# 
+#
 # Ver URL https SSL con curl
 # https://linux.die.net/man/1/curl
 # curl
@@ -11,7 +11,7 @@
 #
 #!/bin/bash
 echo "Uso.: ./urlssl.sh https://url"
-curl -0 -k -s -vvv \
+curl --cacert MyRootCA.crt -0 -k -s -vvv \
 -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
 -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
 -H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
