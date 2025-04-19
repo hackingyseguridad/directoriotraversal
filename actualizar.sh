636a2ceab566296d4abf11300cc579d562a6b800
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-19 20:20:30 +0200

    Create actualizar.sh

---
+++ actualizar.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo
+echo "... actualizando diccionarios path traversal ...  (R) 2025 hackingyseguridad.com "
+echo
+wget https://raw.githubusercontent.com/hackingyseguridad/directoriotraversal/refs/heads/master/pathtraversal.txt --inet4-only
+wc -l pathtraversal.txt
+echo ".."
+echo "..."
+echo "...."
+echo "....."
+echo
+echo "actualizado !!! "
